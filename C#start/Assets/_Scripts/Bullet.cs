e5cd8c0dd7bfdc99c0d7a8b82943744e6d3bc5ea
sangk95 <jwjw1224@naver.com>
2022-07-03 11:56:04 +0900

    Object Pooling, Factory
    Factory 내부에서 구현한 오브젝트 풀링으로 총알 생성

--- C#start/Assets/_Scripts/Bullet.cs
+++ C#start/Assets/_Scripts/Bullet.cs
@@ -1,19 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Bullet : MonoBehaviour
+public class Bullet : RecycleObject
 {
     [SerializeField]
     float speed = 5f;
+    Vector3 targetPosition;
+    bool isActivated = false;
+    public Action<Bullet> Destroyed;
     void Start()
     {
         
+    }
+    public void Activate(Vector3 startPosition, Vector3 targetPosition) 
+    {
+        transform.position = startPosition;
+        this.targetPosition = targetPosition;
+        Vector3 dir = (targetPosition - startPosition).normalized;
+        transform.rotation = Quaternion.LookRotation(transform.forward, dir);
+        isActivated = true;
+    }
+
+    bool IsArrivedToTarget()
+    {
+        float distance = Vector3.Distance(transform.position, targetPosition);
+        return distance < 0.1f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!isActivated)
+            return;
         transform.position += transform.up * speed * Time.deltaTime;
+
+        if(IsArrivedToTarget())
+        {
+            isActivated = false;
+            if(Destroyed != null)
+            {
+                Destroyed(this);
+            }
+        }
     }
 }
--- C#start/Assets/_Scripts/BulletLauncher.cs
+++ C#start/Assets/_Scripts/BulletLauncher.cs
@@ -7,11 +7,42 @@ public class BulletLauncher : MonoBehaviour
     [SerializeField]
     Bullet bulletPrefab;
     Bullet bullet;
+    [SerializeField]
+    Transform firePosition;
+    Factory bulletFactory;
+    [SerializeField]
+    float fireDelay = 0.5f;
+    float elapsedFireTime;
+    bool canShoot = true; 
+    void Start()
+    {
+        bulletFactory = new Factory(bulletPrefab);
+    }
     public void OnFireButtonPressed(Vector3 position)
     {
-        Debug.Log("Fired"+position);
+        if(!canShoot)
+            return;
+
+        bullet = bulletFactory.Get() as Bullet;
+        bullet.Activate(firePosition.position, position);
+        bullet.Destroyed += OnBulletDestroyed;
+        canShoot = false;
+    }
+    void OnBulletDestroyed(Bullet usedBullet) 
+    {
+        usedBullet.Destroyed -= OnBulletDestroyed;
+        bulletFactory.Restore(usedBullet);
     }
     void Update()
     {
+        if(!canShoot)
+        {
+            elapsedFireTime += Time.deltaTime;
+            if(elapsedFireTime >= fireDelay)
+            {
+                canShoot = true;
+                elapsedFireTime = 0f;
+            }
+        }
     }
 }
